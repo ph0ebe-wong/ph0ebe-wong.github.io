770e5ee0d075deafede19c3df507da8345856aeb
ph0ebe-wong <143980276+ph0ebe-wong@users.noreply.github.com>
2024-01-12 00:41:34 +0800

    navbar and landing page

--- src/Components/home.jsx
+++ src/Components/home.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import './Styling/Home.css'
 
 const Home = () =>{
+
+const Name = 'Phoebe Wong'
+
 return (
-    <div>
-        Home
-    </div>
+    <>
+        <div style={{position: 'relative', width: '100%', height: '100vh'}}
+        class="cont"
+        ></div>
+            <section class="section-loader">
+                <h2 class="section__h2">
+                    {Name.split('').map((char, index) => (
+                        <span key={index} className="section__span">{char}</span>
+                    ))}
+                </h2>
+            </section>
+    </>
 )
 }
 export default Home 
--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -1,10 +1,46 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from 'react';
+import './Styling/NavBar.css'
+import Logo from './Assets/portfolio logo.png'
 
-function NavBar(){
-return (
-    <div className="navbar-container">
-         
+const Navbar = () => {
+  const [activeItem, setActiveItem] = useState('home');
+  const indicatorRef = useRef(null);
+
+  const handleItemClick = (item, event) => {
+    setActiveItem(item);
+    updateIndicator(event.target);
+  };
+
+  const updateIndicator = (activeElement) => {
+    const width = activeElement.offsetWidth;
+    const left = activeElement.offsetLeft;
+    indicatorRef.current.style.width = `${width}px`;
+    indicatorRef.current.style.left = `${left}px`;
+  };
+
+  useEffect(() => {
+    const activeElement = document.querySelector('.nav-item.current');
+    if (activeElement) {
+      updateIndicator(activeElement);
+    }
+  }, [activeItem]);
+
+
+  return (
+    <nav class="nav">
+    <div class="container">
+      {/* <h1 class="logo"><a href="#"><i class="fa-solid fa-moon"></i> My Website</a></h1> */}
+      <img className="navbar-logo" src={Logo }/> 
+      <ul>
+        <li><a href="#" className={`nav-item ${activeItem === 'home' ? 'current' : ''}`} onClick={(e) => handleItemClick('home', e)}>Home</a></li>
+        <li><a href="#" className={`nav-item ${activeItem === 'experience' ? 'current' : ''}`} onClick={(e) => handleItemClick('experience', e)}>Experience</a></li>
+        <li><a href="#" className={`nav-item ${activeItem === 'projects' ? 'current' : ''}`} onClick={(e) => handleItemClick('projects', e)}>Projects</a></li>
+      </ul>      
+      <span ref={indicatorRef} className="nav-indicator"></span>
     </div>
-)
-}
-export default NavBar 
+  </nav>  
+  );
+};
+
+
+export default Navbar;
